92d05f3c2b3aeaa8d16707c5bad925e76e92a3f0
BrayanHuesoS <Brayansegura323@gmail.com>
2024-08-26 22:47:33 -0500

    Pagina Lunes 26

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template,url_for
+
 servidor1 = Flask(_name_)
-@servidor1.route('/inicio')
-def home():
+
+@servidor1.route("/inicio")
+def home ():
     return render_template ('template.html')
-    if _name_ == '_main_':
-        servidor1.run(debug=True, port=5001)
+
+if _name== 'main_':
+    servidor1.run(debug=True, port=5001)
